610c87cef5350efa17da0b1f8102a945201e4390
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-20 14:32:02 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        
+        if(a>0 && b<10){
+            System.out.println(c);
+        }
+    }
+}
