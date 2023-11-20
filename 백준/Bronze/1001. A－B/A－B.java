cda18b318fb574b56e00e080e8260de33aa9b251
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-20 14:30:54 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main { public static void main(String[] arg) { 
+    Scanner sc = new Scanner(System.in); 
+    int a = sc.nextInt(); 
+    int b = sc.nextInt(); 
+    int c = a - b;
+
+    if(a>0 && b<10) {
+        System.out.println(c);
+        }
+    }
+}
