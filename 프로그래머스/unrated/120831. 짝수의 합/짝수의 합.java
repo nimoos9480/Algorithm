a63677532cd549aabe2a1c1656f99fca86eff4da
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-20 16:04:11 +0900

    Update 짝수의 합.java

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -10,3 +10,16 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution2 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 0){
+                answer2 += i;
+            }
+        }
+        return answer2;
+    }
+}
