8f81bca27b705fac475dd27bf68cd5044e39b33f
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-21 15:16:05 +0900

    Update 양꼬치.java

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -9,3 +9,11 @@ class Solution {
         return answer;
     }
 }
+
+// 더 간결하게
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 12000 * n + (k - n/10) * 2000;
+        return answer;
+    }
+}
