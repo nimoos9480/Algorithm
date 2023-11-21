6a237772938e71f7ec0626336843ed1e41b6dbf2
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-21 15:57:24 +0900

    Update 뒤에서 5등까지.java

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -11,4 +11,28 @@ class Solution {
 
 
     }
+
+    // 다른 풀이
+    class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        return Arrays.copyOfRange(num_list, 0, 5);
+    }
+}
+
+    // 다른 사람 풀이
+    class Solution {
+
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
+
 }
