9cbf4e8a6a70cf25d821677b6dcca329453ab0ef
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-21 16:56:48 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+
+   Scanner sc = new Scanner(System.in);
+   
+   int A = sc.nextInt();
+   int B = sc.nextInt();
+   int C = sc.nextInt();
+
+	int result1 = (A+B)%C;
+	int result2 = ((A%C) + (B%C))%C;
+	int result3 = (A*B)%C;
+	int result4 = ((A%C) * (B%C))%C;
+
+	if(2 <= A && A<= 10000 && 2 <= B && B<= 10000 && 2 <= C && C<= 10000) {
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+	} 
+
+}
+
+}
