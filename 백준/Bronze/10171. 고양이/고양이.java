a2f499982486e3b374224916325717fae39bf91c
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-27 15:17:43 +0900

    Update 고양이.java

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -5,9 +5,14 @@ public class Main {
         String b = " )  ( ')";
         String c = "(  /  )";
         String d = " \\(__)|";
+	    
         System.out.println(a);
-		 System.out.println(b); 
-		 System.out.println(c); 
-		 System.out.println(d);
+	System.out.println(b); 
+	System.out.println(c); 
+	System.out.println(d);
+
+
+	    // 문자열을 선언할 때 백슬래시 '\'를 사용하려면 이스케이프 문자로 사용해야 함
+	    // ==>  '\'를 나타내기 위해서는 \\로 표현해야 한다
     }
 }
