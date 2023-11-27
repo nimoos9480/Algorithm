424d59a2bca48b32688f31503927bdf00417ad6b
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-11-27 14:13:14 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        // 키큰 사람의 수 count
+        for(int i=0; i<array.length; i++) {
+            if(array[i] > height) {
+                answer++;
+            } else {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
