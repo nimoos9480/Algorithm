bb3092b3cdf99c795b609c6b6d10ea5f6e2577ce
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2023-11-28 00:26:58 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class Main { 
     public static void main(String[] args) { 
-        Scanner sc = new Scanner(System.in); int A = sc.nextInt();
+        Scanner sc = new Scanner(System.in); 
+        int A = sc.nextInt();
 
     // 윤년 조건 확인
     if ((A % 4 == 0 && A % 100 != 0) || (A % 400 == 0)) {
