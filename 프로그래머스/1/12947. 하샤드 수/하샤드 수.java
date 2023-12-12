954260364539068b917164019ca3458eace81421
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2023-12-13 01:05:19 +0900

    Update 하샤드 수.java

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,12 +3,27 @@ class Solution {
         boolean answer = true;
 
         int sum = 0;
-//         // x를 문자열로 전환
-//         String str = Integer.toString(x);
-//         for(int i=0; i<str.length(); i++) {
-//             int num = str.charAt(i) - '0';
-//             sum += num;
-//         }     
+        // x를 문자열로 전환
+        String str = Integer.toString(x);
+        for(int i=0; i<str.length(); i++) {
+            int num = str.charAt(i) - '0';
+            sum += num;
+        }     
+
+        
+        if(num % sum == 0) {
+            return answer;
+        }
+        return false;
+    }
+}
+
+// 다른 풀이
+class Solution2 {
+    public boolean solution(int x) {
+        boolean answer = true;
+
+        int sum = 0;   
         int num = x;
          while(x>0) {
             sum += x % 10;
