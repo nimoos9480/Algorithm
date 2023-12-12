28a705759585a27882b5768289eda7c599a3b7d6
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2023-12-13 01:02:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,16 +3,20 @@ class Solution {
         boolean answer = true;
 
         int sum = 0;
-        // x를 문자열로 전환
-        String str = Integer.toString(x);
-        for(int i=0; i<str.length(); i++) {
-            int num = str.charAt(i) - '0';
-            sum += num;
+//         // x를 문자열로 전환
+//         String str = Integer.toString(x);
+//         for(int i=0; i<str.length(); i++) {
+//             int num = str.charAt(i) - '0';
+//             sum += num;
+//         }     
+        int num = x;
+         while(x>0) {
+            sum += x % 10;
+            x /= 10;
         }
         
-        
-        if(sum != 0 && x % sum == 0) {
-            return answer;
+        if(num % sum == 0) {
+            return true;
         }
         return false;
     }
