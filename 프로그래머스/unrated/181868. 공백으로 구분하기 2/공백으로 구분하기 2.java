aa095b076cdec3fc2fe4b6ba9ae6322a48e6bba1
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2023-12-22 15:24:40 +0900

    Update 공백으로 구분하기 2.java

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,6 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        
+        // ''\\s+'는 하나 이상의 공백
         String[] answer = my_string.trim().split("\\s+");
         
         return answer;
