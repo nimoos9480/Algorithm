611e423adefed0e0dfc4526cb023a69c654bafd7
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2024-01-09 21:06:34 +0900

    Update 접미사인지 확인하기.java

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -7,3 +7,9 @@ class Solution {
         return answer;
     }
 }
+
+// endsWith() 함수는 대상 문자열이 특정 문자 또는 문자열로 끝나는지 체크하는 함수이다.
+// - 해당 문자열로 끝나는지 여부를 확인하고 boolean에 맞춰 true/false 값을 리턴한다.
+
+// 추가) startsWith() 함수는 대상 문자열이 특정 문자 또는 문자열로 시작하는지 체크하는 함수이다.
+// - 해당 문자열로 시작하는지 여부를 확인하고 boolean에 맞춰 true/false 값을 리턴한다.
