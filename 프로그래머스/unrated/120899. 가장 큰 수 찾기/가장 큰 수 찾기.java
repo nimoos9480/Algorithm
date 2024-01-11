b2da86d263bdbf81bc9d7fbaf307f59b2ba7dca3
Shin Soomin <133620313+nimoos9480@users.noreply.github.com>
2024-01-11 19:18:03 +0900

    [level unrated] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int max = array[0];
+        int index = 0;
+        
+        for(int i=0; i<array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+        
+        int[] answer = {max, index};
+        return answer;
+    }
+}
